ddf71c718dba58a8f208d3076e18954d57216c5b
appleleir <buptylei@163.com>
2016-11-26 17:24:58 +0800

    归并排序

---
+++ mergesort.py
@@ -0,0 +1,21 @@
+#/usr/bin/env python
+"mergesort"
+N =[1,2,3,4,2,3,4,3,5,23]
+
+def mergesort(seq):
+    mid = len(seq) // 2
+    left,right = seq[:mid],seq[mid:]
+    if len(left)>1: left= mergesort(left)
+    if len(right)>1:right=mergesort(right)
+    res =[]
+    while left and right:
+        if left[-1] >= right[-1]:
+            res.append(left.pop())
+        else:
+            res.append(right.pop())
+    res.reverse()         
+    return left+right+res
+
+if __name__=="__main__":
+   print mergesort(N)
+	
